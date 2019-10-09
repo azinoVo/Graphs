d8c0e5e53d4b2bec6fe6963a47dca3365c1c56e2
azinoVo <azinovo22@gmail.com>
2019-10-09 17:43:58 -0500

    initial commit social

---
+++ projects/islands.py
@@ -0,0 +1,45 @@
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+class Graph():
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+
+    def add_edges(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist")
+
+islands = [[1, 0, 0, 1, 1, 0, 1, 1, 0, 1],
+           [0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
+           [0, 1, 1, 1, 0, 0, 0, 1, 0, 1],
+           [0, 0, 1, 0, 0, 1, 0, 0, 1, 1],
+           [0, 0, 1, 1, 0, 1, 0, 1, 1, 0],
+           [0, 1, 0, 1, 1, 1, 0, 1, 0, 0],
+           [0, 0, 1, 0, 0, 1, 1, 0, 0, 0],
+           [1, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+           [0, 1, 1, 0, 0, 0, 1, 1, 0, 0],
+           [0, 0, 1, 1, 0, 1, 0, 0, 1, 0]]
+
+
+#-------------------------------------------------------------------------
+# Alternate solution does not turn the matrix into a graph
+
+#-------------------------------------------------------------------------
+# Another solution can be done with a recursive function that turns
+# adjacent land into water or 1 to 0
